e7369775e867d4eddd136a56d1f05e85e7c405c6
jose-pedro <josepedro.bernardes1@gmail.com>
2024-02-12 18:22:26 -0300

    Feat (New Entity): DevEventSpeaker

--- AwesomeDevEvents/AwesomeDevEvents.API/Entities/DevEventSpeaker.cs
+++ AwesomeDevEvents/AwesomeDevEvents.API/Entities/DevEventSpeaker.cs
@@ -2,5 +2,10 @@
 {
     public class DevEventSpeaker
     {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public string TalkTitle { get; set; }
+        public string TalkDescription { get; set; }
+        public string LinkedInProfile { get; set;}
     }
 }
