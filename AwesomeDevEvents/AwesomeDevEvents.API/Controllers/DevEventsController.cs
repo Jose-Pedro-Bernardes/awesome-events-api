0fbbc0c7303b8df8e633450f8816899389db4896
jose-pedro <josepedro.bernardes1@gmail.com>
2024-02-13 12:12:08 -0300

    Feat: CRUD DevEvents.

--- AwesomeDevEvents/AwesomeDevEvents.API/Controllers/DevEventsController.cs
+++ AwesomeDevEvents/AwesomeDevEvents.API/Controllers/DevEventsController.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using AwesomeDevEvents.API.Entities;
+using AwesomeDevEvents.API.Persistence;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 namespace AwesomeDevEvents.API.Controllers
@@ -7,5 +9,69 @@ namespace AwesomeDevEvents.API.Controllers
     [ApiController]
     public class DevEventsController : ControllerBase
     {
+        private readonly DevEventsDbContext _context;
+        public DevEventsController(DevEventsDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            var devEvents = _context.DevEvents.Where(d => !d.IsDeleted).ToList();
+
+            return Ok(devEvents);
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetById(Guid id)
+        {
+            var devEvents = _context.DevEvents.SingleOrDefault(d => d.Id == id);
+
+            if(devEvents == null) 
+            {
+                return NotFound();
+            }
+
+            return Ok(devEvents);
+        }
+
+        [HttpPost]
+        public IActionResult Post(DevEvent devEvent)
+        {
+            _context.DevEvents.Add(devEvent);
+
+            return CreatedAtAction(nameof(GetById), new { id = devEvent.Id }, devEvent);
+        }
+
+        [HttpPut("{id}")]
+        public IActionResult Update(Guid id, DevEvent input)
+        {
+            var devEvent = _context.DevEvents.SingleOrDefault(d => d.Id == id);
+
+            if(devEvent == null)
+            {
+                return NotFound();
+            }
+
+            devEvent.Update(input.Title, input.Description, input.StartDate, input.EndDate);
+
+            return NoContent();
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult Delete(Guid id)
+        {
+            var devEvent = _context.DevEvents.SingleOrDefault(d => d.Id == id);
+
+            if (devEvent == null)
+            {
+                return NotFound();
+            }
+
+            devEvent.Delete();
+
+            return NoContent();
+        }
     }
 }
